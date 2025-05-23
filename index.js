8887788586ee299383f8ecef1a07888ee4d6c0cc
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-05-22 21:12:25 -0500

    Updated JS file to add content to the HTML file rather than hard coded HTML. Changed css file name from index to styles.

--- index.js
+++ index.js
@@ -27,3 +27,45 @@ const posts = [
         likes: 152
     }
 ]
+
+let main = document.querySelector("main")
+
+let postContent = document.createElement('section')
+postContent.classList.add('post-content')
+let postAuthorDiv = document.createElement('div')
+postAuthorDiv.classList.add('post-author')
+
+main.append(postContent)
+
+let postSection = `
+    <section class="post-content">
+      <div class="post-author">
+        <img class="avatar" src="${posts[0].avatar}" alt="" />
+        <div class="post-author-details">
+          <p class="post-author-name">${posts[0].name}</p>
+          <p class="post-author-location">${posts[0].location}</p>
+        </div>
+      </div>
+
+      <div class="post-img">
+        <img src="${posts[0].post}" alt="" class="post-image" />
+      </div>
+
+      <div class="post-body">
+        <div class="post-actions">
+          <img src="/images/icon-heart.png" alt="" />
+          <img src="/images/icon-comment.png" alt="" />
+          <img src="/images/icon-dm.png" alt="" />
+        </div>
+        <div class="post-metrics">
+          <p>${posts[0].likes}</p>
+        </div>
+        <div class="post-comments">
+          <p>
+            <span class="bold">${posts[0].username}</span> ${posts[0].comment}
+          </p>
+        </div>
+      </div>
+    </section>`
+
+main.innerHTML = postSection
